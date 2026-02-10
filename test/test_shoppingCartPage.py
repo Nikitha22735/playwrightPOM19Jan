a52674075851a37ebce8bb9004e56e3b87bc0191
Nikitha22735 <nikitha.thripuram@gmail.com>
2026-02-10 09:32:18 +0530

    added conftest

--- test/test_shoppingCartPage.py
+++ test/test_shoppingCartPage.py
@@ -1,14 +1,13 @@
-# from playwright.sync_api import 
 from pages.homeaPage import homePage
 from pages.resultsPage import resultsPage
 import pytest
 
 @pytest.mark.single2()
-def test_ValdiateTheShoppingCartDetails(page,loginToAmazon):
-    homePageObj = homePage(page)
+def test_ValdiateTheShoppingCartDetails(page,loginToAmazon,homePageObj,resultsPageObj):
+    # homePageObj = homePage(page)
     homePageObj.enterProductToSearch("iphone")
     homePageObj.clickOnSearchBtn()
-    resultsPageObj = resultsPage(page)
+    # resultsPageObj = resultsPage(page)
     resultsPageObj.validateTheVisibilityOfResults() 
     title = "Sponsored Ad - iPhone 15 Plus (512 GB) - Green"
     resultsPageObj.clickOnAddToCartBtn(title)
@@ -17,6 +16,17 @@ def test_ValdiateTheShoppingCartDetails(page,loginToAmazon):
     resultsPageObj.clickOnAddToCartBtn(title)
     resultsPageObj.clickOnCartBtn()
     page.wait_for_timeout(5000)
+
+
+def test_ValdiateTheShoppingCartDetails12(page,loginToAmazon,homePageObj):
+    # homePageObj = homePage(page)
+    homePageObj.enterProductToSearch("iphone")
+    homePageObj.clickOnSearchBtn()
+
+
+def test3(page):
+    homePageObj = homePage(page)
+
     
 
 
